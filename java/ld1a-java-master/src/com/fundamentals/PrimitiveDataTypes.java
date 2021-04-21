00c81c1fabb4a1674065a21f42b4f79d4585150c
livegit2 <livegit2@gmail.com>
2021-04-21 08:27:37 +0530

    commit

---
+++ java/ld1a-java-master/src/com/fundamentals/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+package com.fundamentals;
+
+public class PrimitiveDataTypes {
+	byte bvar;
+	short shortvar;
+	int intvar;
+	long longvar;
+	float floatvar;
+	double doublevar;
+	char charvar;
+	boolean booleanvar;
+
+	public static void main(String[] args) {
+//		create a reference
+		PrimitiveDataTypes primitiveDataTypes;
+//		create the memory
+		primitiveDataTypes = new PrimitiveDataTypes();
+		System.out.println(primitiveDataTypes.bvar);
+		System.out.println(primitiveDataTypes.shortvar);
+		System.out.println(primitiveDataTypes.intvar);
+		System.out.println(primitiveDataTypes.floatvar);
+		System.out.println(primitiveDataTypes.doublevar);
+		System.out.println(primitiveDataTypes.charvar);
+		System.out.println(primitiveDataTypes.booleanvar);
+	}
+}
