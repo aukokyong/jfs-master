bf778ec14f19788bf3f5b380cddb203e9ed03d00
livegit2 <livegit2@gmail.com>
2021-05-04 12:21:47 +0530

    commit

--- java/lmp7a-sb-rest-crud-1/src/main/java/com/domain/controller/ProductController.java
+++ java/lmp7a-sb-rest-crud-1/src/main/java/com/domain/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.domain.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +23,50 @@ import com.domain.utility.Colors;
 public class ProductController {
 	@Autowired
 	ProductRepository productRepository;
+	//@RequestMapping(value = "/products", method = RequestMethod.GET)
+	//@GetMapping(value = "/products")
 	@GetMapping("/products")
-	public List<Product> getAllProducts() {
+	public List<Product> findAllProducts() {
 		List<Product> productsList =  productRepository.findAll();
 		productsList.forEach(Colors::pc);
 		return productsList;
 	}
+	// @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+	@GetMapping("/products/{id}")
+	public Product findProductById(@PathVariable(value="id") Long productId) {
+		Optional<Product> optinalProduct = productRepository.findById(productId);
+		Product product = optinalProduct.get();
+		Colors.pc(product);
+		return product;
+	}
+	//@RequestMapping(value = "/products", method = RequestMethod.POST)
+	@PostMapping("/products")
+	public Product createProduct(@RequestBody Product product) {
+	      Colors.pc("Inserting " + product);
+	      Product productReturned = productRepository.save(product);
+	      Colors.pc("Inserted " + product);
+		return productReturned;
+	}
+	   //@RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+	@PutMapping("/products/{id}")
+	public Product updateProduct(@PathVariable(value="id") Long productId, @RequestBody Product product) {
+		Optional<Product> optinalProduct = productRepository.findById(productId);
+		Product productFromDB = optinalProduct.get();
+		productFromDB.setProductName(product.getProductName());
+		productFromDB.setDescription(product.getDescription());
+		productFromDB.setPrice(product.getPrice());
+	      Colors.pc("Updating " + product);
+	      Product productReturned = productRepository.save(productFromDB);
+	      Colors.pc("Updated " + product);
+		return productReturned;
+	}
+
+	   // @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+	   @DeleteMapping("/products/{id}")
+	   public boolean deleteProduct(@PathVariable(value = "id") Long productId) {
+	      productRepository.deleteById(productId);
+	      Colors.pc("Deleted " + productId);
+	      return true;
+	   }
+	
 }
